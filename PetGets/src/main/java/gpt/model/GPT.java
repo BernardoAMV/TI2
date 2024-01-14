61a81b84a6b3c4d7c0eef989c50d10df981725d2
BernardoAMV <125494665+BernardoAMV@users.noreply.github.com>
2024-01-14 17:08:44 -0300

    Update GPT.java

--- PetGets/src/main/java/gpt/model/GPT.java
+++ PetGets/src/main/java/gpt/model/GPT.java
@@ -2,7 +2,6 @@ package gpt.model;
 
 public class GPT{
 	
-	private String API_KEY = "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";
 	private String url = "https://api.openai.com/v1/chat/completions";
 	private String model = "gpt-3.5-turbo";
 	
